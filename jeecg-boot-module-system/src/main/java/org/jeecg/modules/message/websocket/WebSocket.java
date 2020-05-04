8f269c960c9364f6957b7557ccf0868ebde84660
Gitjiangwei <15035571514@163.com>
2020-05-04 16:00:17 +0800

    webSocket修改

--- jeecg-boot-module-system/src/main/java/org/jeecg/modules/message/websocket/WebSocket.java
+++ jeecg-boot-module-system/src/main/java/org/jeecg/modules/message/websocket/WebSocket.java
@@ -1,6 +1,8 @@
 package org.jeecg.modules.message.websocket;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CopyOnWriteArraySet;
 
@@ -11,6 +13,10 @@ import javax.websocket.Session;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
+import org.jeecg.modules.message.mapper.SysUserShopMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
 import com.alibaba.fastjson.JSONObject;
@@ -26,83 +32,110 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @ServerEndpoint("/websocket/{userId}") //此注解相当于设置访问URL
 public class WebSocket {
-    
+
+    /**
+     * 此处是解决无法注入的关键
+     */
+    private static ApplicationContext applicationContext;
+
+
+    private static SysUserShopMapper sysUserShopMapper;
+
     private Session session;
-    
-    private static CopyOnWriteArraySet<WebSocket> webSockets =new CopyOnWriteArraySet<>();
-    private static Map<String,Session> sessionPool = new HashMap<String,Session>();
-    
-    @OnOpen
-    public void onOpen(Session session, @PathParam(value="userId")String userId) {
-        try {
-			this.session = session;
-			webSockets.add(this);
-			sessionPool.put(userId, session);
-			log.info("【websocket消息】有新的连接，总数为:"+webSockets.size());
-		} catch (Exception e) {
-		}
+
+    private static CopyOnWriteArraySet<WebSocket> webSockets = new CopyOnWriteArraySet<>();
+    private static Map<String, Session> sessionPool = new HashMap<String, Session>();
+
+    @Autowired
+    public void setSysUserShopMapper(SysUserShopMapper sysUserShopMapper){
+        WebSocket.sysUserShopMapper = sysUserShopMapper;
     }
-    
+
+    public static void setApplicationContext(ApplicationContext applicationContext){
+        WebSocket.applicationContext = applicationContext;
+    }
+
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam(value = "userId") String userId) {
+        try {
+            this.session = session;
+            webSockets.add(this);
+            sessionPool.put(userId, session);
+            sysUserShopMapper.updateOnline(userId,"1");
+            log.info("【websocket消息】有新的连接，总数为:" + webSockets.size());
+        } catch (Exception e) {
+        }
+    }
+
     @OnClose
     public void onClose() {
         try {
-			webSockets.remove(this);
-			log.info("【websocket消息】连接断开，总数为:"+webSockets.size());
-		} catch (Exception e) {
-		}
+            webSockets.remove(this);
+            String userId = this.session.getPathParameters().get("userId");
+            sysUserShopMapper.updateOnline(userId,"0");
+            log.info("【websocket消息】连接断开，总数为:" + webSockets.size());
+        } catch (Exception e) {
+        }
     }
-    
+
     @OnMessage
     public void onMessage(String message) {
         //todo 现在有个定时任务刷，应该去掉
-    	log.debug("【websocket消息】收到客户端消息:"+message);
-    	JSONObject obj = new JSONObject();
-    	obj.put("cmd", "heartcheck");//业务类型
-    	obj.put("msgTxt", "心跳响应");//消息内容
-    	session.getAsyncRemote().sendText(obj.toJSONString());
+        log.debug("【websocket消息】收到客户端消息:" + message);
+        JSONObject obj = new JSONObject();
+        obj.put("cmd", "heartcheck");//业务类型
+        obj.put("msgTxt", "心跳响应");//消息内容
+        session.getAsyncRemote().sendText(obj.toJSONString());
     }
-    
+
     // 此为广播消息
     public void sendAllMessage(String message) {
-    	log.info("【websocket消息】广播消息:"+message);
-        for(WebSocket webSocket : webSockets) {
+        log.info("【websocket消息】广播消息:" + message);
+        for (WebSocket webSocket : webSockets) {
             try {
-            	if(webSocket.session.isOpen()) {
-            		webSocket.session.getAsyncRemote().sendText(message);
-            	}
+                if (webSocket.session.isOpen()) {
+                    webSocket.session.getAsyncRemote().sendText(message);
+                }
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
-    
+
     // 此为单点消息
     public void sendOneMessage(String userId, String message) {
         Session session = sessionPool.get(userId);
-        if (session != null&&session.isOpen()) {
+        if (session != null && session.isOpen()) {
             try {
-            	log.info("【websocket消息】 单点消息:"+message);
+                log.info("【websocket消息】 单点消息:" + message);
                 session.getAsyncRemote().sendText(message);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
-    
+
     // 此为单点消息(多人)
     public void sendMoreMessage(String[] userIds, String message) {
-    	for(String userId:userIds) {
-    		Session session = sessionPool.get(userId);
-            if (session != null&&session.isOpen()) {
+        for (String userId : userIds) {
+            Session session = sessionPool.get(userId);
+            if (session != null && session.isOpen()) {
                 try {
-                	log.info("【websocket消息】 单点消息:"+message);
+                    log.info("【websocket消息】 单点消息:" + message);
                     session.getAsyncRemote().sendText(message);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
-    	}
-        
+        }
+
     }
-    
+
+    public void sendMerchantMessage(){
+        for (int i =0; i<webSockets.size(); i++){
+
+        }
+    }
+
 }
