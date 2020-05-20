3d52dbd7aa4149651b2c2135564a6fe7444380df
Backlight-NI <29359047@qq.com>
2020-05-20 16:46:18 +0800

    by nyx 修改模板更新为json解析方式

---
+++ jeecg-boot-module-system/src/main/java/org/kunze/diansh/service/IWXPayService.java
@@ -0,0 +1,18 @@
+package org.kunze.diansh.service;
+
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.jeecg.common.api.vo.Result;
+import org.kunze.diansh.entity.Order;
+
+import java.util.Map;
+
+public interface IWXPayService  extends IService<Order> {
+
+    //小程序统一下单
+    Map<String, String> wxAppCreatePayOrder(String orderId, String money, String code);
+
+    //查询微信订单状态
+    Result qryWxOrderStatus(String outTradeNo);
+
+}
