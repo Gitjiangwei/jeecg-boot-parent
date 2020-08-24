c37eeb6ade3b90319c0c81a728eb9a77e15eca6a
Gitjiangwei <15035571514@163.com>
2020-08-24 16:35:48 +0800

    修改骑手

--- jeecg-boot-module-system/src/main/java/org/kunze/diansh/service/impl/DistributionServiceImpl.java
+++ jeecg-boot-module-system/src/main/java/org/kunze/diansh/service/impl/DistributionServiceImpl.java
@@ -90,36 +90,34 @@ public class DistributionServiceImpl extends ServiceImpl<DistributionMapper, Dis
         if(riderList.size() == 0) {
             rider.setStatus("1");
             riderList = riderMapper.queryRiderList(rider);
-        }else{
             int ran1 = 0;
-            if(riderList.size()>1){
+            if (riderList.size() > 1) {
                 Random r = new Random();
                 ran1 = r.nextInt(riderList.size());
             }
             RiderVo riderVo = riderList.get(ran1);
             //添加配送信息
             Distribution distribution = new Distribution();
-            distribution.setId(UUID.randomUUID().toString().replace("-","")); //主键ID
-            distribution.setAddress(address.getProvince()+address.getCity()+address.getCounty()+address.getStreet()); //配送地址
+            distribution.setId(UUID.randomUUID().toString().replace("-", "")); //主键ID
+            distribution.setAddress(address.getProvince() + address.getCity() + address.getCounty() + address.getStreet()); //配送地址
             distribution.setOrderId(orderId); //订单号
             distribution.setRiderId(riderVo.getId()); //骑手id
             distribution.setShopId(shop.getId()); //超市id
             distribution.setDeliveryFee(CalculationUtil.MetaconversionScore(deliveryFee)); //配送费
             distribution.setPickNo(order.getPickNo()); //取单号
-            riderMapper.editRiderNum("1",null,riderVo.getId());
+            riderMapper.editRiderNum("1", null, riderVo.getId());
             //发送短信
-            Boolean a = SendSms.sendSms(riderVo.getTelphone(), orderId+","+list.get(0).getAddressTotal()+list.get(0).getShopName()+","+distribution.getPickNo(), SendSmsEnum.NOTIC_DISTRIBUTION_RIDER);
-            if(!a){
+            Boolean a = SendSms.sendSms(riderVo.getTelphone(), orderId + "," + list.get(0).getAddressTotal() + list.get(0).getShopName() + "," + distribution.getPickNo(), SendSmsEnum.NOTIC_DISTRIBUTION_RIDER);
+            if (!a) {
                 System.out.println("短信发送失败！");
                 return false;
-            }else {
+            } else {
                 int result = distributionMapper.saveDistribution(distribution);
-                if(result>0){
+                if (result > 0) {
                     isFlag = true;
                 }
             }
         }
-
         return isFlag;
     }
 
