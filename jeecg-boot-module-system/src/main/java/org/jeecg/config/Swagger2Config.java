1a7c986c6636230e5f42d24f0e09c3a63e6b9a24
JW <15035571514@163.com>
2020-09-24 15:03:31 +0800

    swagger修改配置

--- jeecg-boot-module-system/src/main/java/org/jeecg/config/Swagger2Config.java
+++ jeecg-boot-module-system/src/main/java/org/jeecg/config/Swagger2Config.java
@@ -33,7 +33,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import springfox.documentation.RequestHandler;
 
 /**
- * @Author scott
+ * swagger 可视化界面配置
+ *
+ * @author 姜伟
+ * @date 2020/9/20
  */
 @Slf4j
 @Configuration
@@ -41,110 +44,109 @@ import springfox.documentation.RequestHandler;
 @EnableSwaggerBootstrapUI
 public class Swagger2Config implements WebMvcConfigurer {
 
-	// 定义分隔符,配置Swagger多包
-	private static final String splitor = ";";
-	/**
-	 *
-	 * 显示swagger-ui.html文档展示页，还必须注入swagger资源：
-	 * 
-	 * @param registry
-	 */
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-		registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
-		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-	}
+    // 定义分隔符,配置Swagger多包
+    private static final String SOLITOR = ";";
 
-	/**
-	 * swagger2的配置文件，这里可以配置swagger2的一些基本的内容，比如扫描的包等等
-	 *
-	 * @return Docket
-	 */
-	@Bean
-	public Docket createRestApi() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.apiInfo(apiInfo())
-				.select()
-				//此包路径下的类，才生成接口文档
-				//.apis(RequestHandlerSelectors.basePackage("org.jeecg.modules"))
-				.apis(basePackage("org.jeecg.modules"+splitor+"org.kunze.diansh"+splitor+"org.alipay"))
-				//加了ApiOperation注解的类，才生成接口文档
-	            .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
-				.paths(PathSelectors.any())
-				.build()
-				.securitySchemes(Collections.singletonList(securityScheme()));
-				//.globalOperationParameters(setHeaderToken());
-	}
+    /**
+     *
+     * 显示swagger-ui.html文档展示页，还必须注入swagger资源：
+     * 
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 
-	/**
-	 * 重写basePackage方法，使能够实现多包访问
-	 * @author  teavamc
-	 * @date 2019/1/26
-	 * @param [basePackage]
-	 * @return com.google.common.base.Predicate<springfox.documentation.RequestHandler>
-	 */
-	public static Predicate<RequestHandler> basePackage(final String basePackage) {
-		return input -> declaringClass(input).transform(handlerPackage(basePackage)).or(true);
-	}
+    /**
+     * swagger2的配置文件，这里可以配置swagger2的一些基本的内容，比如扫描的包等等
+     *
+     * @return Docket
+     */
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+                // 此包路径下的类，才生成接口文档
+                // .apis(RequestHandlerSelectors.basePackage("org.jeecg.modules"))
+                .apis(basePackage("org.jeecg.modules" + SOLITOR + "org.kunze.diansh" + SOLITOR + "org.alipay"))
+                // 加了ApiOperation注解的类，才生成接口文档
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)).paths(PathSelectors.any())
+                .build().securitySchemes(Collections.singletonList(securityScheme()));
+        // .globalOperationParameters(setHeaderToken());
+    }
 
-	private static Function<Class<?>, Boolean> handlerPackage(final String basePackage)     {
-		return input -> {
-			// 循环判断匹配
-			for (String strPackage : basePackage.split(splitor)) {
-				boolean isMatch = input.getPackage().getName().startsWith(strPackage);
-				if (isMatch) {
-					return true;
-				}
-			}
-			return false;
-		};
-	}
-	private static Optional<? extends Class<?>> declaringClass(RequestHandler input) {
-		return Optional.fromNullable(input.declaringClass());
-	}
+    /**
+     * 重写basePackage方法，使能够实现多包访问
+     * 
+     * @author teavamc
+     * @date 2019/1/26
+     * @param [basePackage]
+     * @return com.google.common.base.Predicate<springfox.documentation.RequestHandler>
+     */
+    public static Predicate< RequestHandler > basePackage(final String basePackage) {
+        return input -> declaringClass(input).transform(handlerPackage(basePackage)).or(true);
+    }
 
-	/***
-	 * oauth2配置
-	 * 需要增加swagger授权回调地址
-	 * http://localhost:8888/webjars/springfox-swagger-ui/o2c.html
-	 * @return
-	 */
-	@Bean
-	SecurityScheme securityScheme() {
-		return new ApiKey(DefContants.X_ACCESS_TOKEN, DefContants.X_ACCESS_TOKEN, "header");
-	}
-	/**
-	 * JWT token
-	 * @return
-	 */
-	private List<Parameter> setHeaderToken() {
+    private static Function< Class< ? >, Boolean > handlerPackage(final String basePackage) {
+        return input -> {
+            // 循环判断匹配
+            for (String strPackage : basePackage.split(SOLITOR)) {
+                boolean isMatch = input.getPackage().getName().startsWith(strPackage);
+                if (isMatch) {
+                    return true;
+                }
+            }
+            return false;
+        };
+    }
+
+    private static Optional< ? extends Class< ? > > declaringClass(RequestHandler input) {
+        return Optional.fromNullable(input.declaringClass());
+    }
+
+    /***
+     * oauth2配置 需要增加swagger授权回调地址 http://localhost:8888/webjars/springfox-swagger-ui/o2c.html
+     * 
+     * @return
+     */
+    @Bean
+    SecurityScheme securityScheme() {
+        return new ApiKey(DefContants.X_ACCESS_TOKEN, DefContants.X_ACCESS_TOKEN, "header");
+    }
+
+    /**
+     * JWT token
+     * 
+     * @return
+     */
+    private List< Parameter > setHeaderToken() {
         ParameterBuilder tokenPar = new ParameterBuilder();
-        List<Parameter> pars = new ArrayList<>();
-        tokenPar.name(DefContants.X_ACCESS_TOKEN).description("token").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+        List< Parameter > pars = new ArrayList<>();
+        tokenPar.name(DefContants.X_ACCESS_TOKEN).description("token").modelRef(new ModelRef("string"))
+                .parameterType("header").required(false).build();
         pars.add(tokenPar.build());
         return pars;
     }
 
-	/**
-	 * api文档的详细信息函数,注意这里的注解引用的是哪个
-	 *
-	 * @return
-	 */
-	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder()
-				// //大标题
-				.title("Jeecg-Boot 后台服务API接口文档")
-				// 版本号
-				.version("1.0")
-//				.termsOfServiceUrl("NO terms of service")
-				// 描述
-				.description("后台API接口")
-				// 作者
-				.contact("JEECG团队")
-                .license("The Apache License, Version 2.0")
-                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
-				.build();
-	}
+    /**
+     * api文档的详细信息函数,注意这里的注解引用的是哪个
+     *
+     * @return
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                // //大标题
+                .title("哄哄到家后台服务API接口文档")
+                // 版本号
+                .version("1.0")
+                // .termsOfServiceUrl("NO terms of service")
+                // 描述
+                .description("哄哄到家后台API接口")
+                // 作者
+                .contact("JEECG团队").license("The Apache License, Version 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html").build();
+    }
 
 }
